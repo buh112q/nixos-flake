2009dbceaad7e3392c10b432696b3464876b131a
buh112q <lnwpoom.zaa123@gmail.com>
2026-03-03 23:46:35 +0700

    uhh

--- configuration.nix
+++ configuration.nix
@@ -71,7 +71,7 @@
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  networking.firewall.enable = false;
   system.stateVersion = "25.11";
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 }
